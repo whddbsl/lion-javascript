e9efd23e885820d915d7d26e53affebd31bbefcb
kindtiger <seonbeom2@gmail.com>
2023-07-07 15:52:57 +0900

    add. 5일차 수업
    - for..of
    - function declaration (rem함수 만들기)

--- client/chapter/core/09-4.loop.js
+++ client/chapter/core/09-4.loop.js
@@ -16,7 +16,7 @@ const javaScript = {
 
 // in 문 : 객체 안에 내가 원하는 값(property)이(가) 있어?
 
-const key = 'createAt';
+const key = 'nickName';
 
 
 // console.log( key in javaScript );
@@ -25,6 +25,7 @@ const key = 'createAt';
 // - 모든 객체가 사용 가능하도록 속성이 확장되었을 때 포함 여부 결과는?
 
 Object.prototype.nickName = 'tiger';
+// Object.prototype.hasOwnProperty = 'a'
 
 // 객체 자신의 속성인지 확인하는 방법
 // - "자신(own)의 속성(property)을 가지고(has)있는지 확인 방법"이 덮어쓰여질 수 있는 위험에 대처하는 안전한 방법은?
@@ -33,33 +34,61 @@ Object.prototype.nickName = 'tiger';
 // console.log( javaScript.hasOwnProperty(key) );
 
 
-console.log( Object.prototype.hasOwnProperty.call(javaScript,key) );
-
-
-
-
-
-
-
-
-
-
-
-
-// for(let key in javaScript){
-//   console.log(key);
-// }
-
-
-
-
-
+// console.log( Object.prototype.hasOwnProperty.call(javaScript,key) );
 
 
 
 // for ~ in 문
 // - 객체 자신의 속성만 순환하려면?
+for(let key in javaScript){
+
+  if({}.hasOwnProperty.call(javaScript,key)){
+
+    console.log(key);
+  }
+}
+
+
+
 // - 배열 객체 순환에 사용할 경우?
+const tens = [10,100,1000,10000];
+
+for(let key in tens){
+  console.log( tens[key] );
+}
+
+
+// for...in 객체를 순환하는 용도로 사용이 가능함.
+// 배열은..좀 그래요.. for...in은 객체에 양보하세요..
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
