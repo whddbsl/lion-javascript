742eb1beb574a6acd1938f3384ec004d5d3a313c
kindtiger <seonbeom2@gmail.com>
2023-07-13 15:52:07 +0900

    add. 9일차 수업
    - 옵셔널 체이닝
    - 원시값과 래퍼객체
    - 숫자 타입
    - 문자 타입
    - 배열 타입

--- client/chapter/core/14-2.closure.js
+++ client/chapter/core/14-2.closure.js
@@ -42,8 +42,6 @@ function handleClick(){
   
     isClicked = !isClicked;
   }
-
- 
 }
 
 
--- client/main.js
+++ client/main.js
@@ -1,2 +1,41 @@
 // console.log('hello js!');
 // confirm('정말 지울꺼야..?');
+
+
+
+
+const input = document.querySelector('#todo');
+
+let value = input.value;
+
+input.addEventListener('input',()=>{
+  value = input.value;
+  console.log(value === 'hello');
+})
+
+
+
+// input.classList.add('is-active');
+// input.classList.remove('is-active');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
