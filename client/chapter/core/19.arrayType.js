3d1445bb95fe828a2730f0d4729035c4e6b0a4f8
kindtiger <seonbeom2@gmail.com>
2023-07-17 11:52:41 +0900

    add. 10일차 수업
    - arrayMethod
    - try..catch (예외처리)

--- client/chapter/core/19.arrayType.js
+++ client/chapter/core/19.arrayType.js
@@ -47,9 +47,25 @@ console.log(friends);
 // 배열 요소 순환(loop)
 // for 문, for ~ of문
 
+for(let i = 0; i < friends.length; i++){
+  // console.log(friends[i]);
+}
+
+
+for(let value of friends){
+  // console.log( value );
+}
+
+
+
 
 // 배열 복사
-let copiedArray;
+// let copiedArray = [...friends];
+// let copiedArray = friends.slice();
+let copiedArray = friends.toSorted();
+
+console.log( copiedArray );
+
 
 
 // 다차원 배열
@@ -63,7 +79,7 @@ const matrix = [
 
 // 행렬의 정중앙에 위치한 요소를 찾으려면?
 
-
+matrix[1][1]
 
 
 
