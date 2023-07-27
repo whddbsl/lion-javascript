4f06d1f1ade4aa160a2bf891d663d0dd04dd8f89
whddbsl <whddbs04140@naver.com>
2023-07-27 15:44:44 +0900

    07.storage

--- client/main.js
+++ client/main.js
@@ -1,3 +1,20 @@
-console.log('hello js!');
+// console.log('hello js!');
 
+import { getNode, getStorage, setStorage } from './lib/index.js';
 
+const textField = getNode('#textField');
+
+function handleTextField() {
+  const value = this.value;
+
+  setStorage('text', value);
+}
+
+function init() {
+  getStorage('text').then((res) => {
+    textField.value = res;
+  });
+}
+
+textField.addEventListener('input', handleTextField);
+window.addEventListener('DOMContentLoaded', init);
